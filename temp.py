9c6de80d3df77125a82aa4999ccab6a30e160b69
WangAnthony3 <41981705+WangAnthony3@users.noreply.github.com>
2018-08-01 00:29:02 -0500

    Change plotting range

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+X = np.linspace(-np.pi*2, np.pi*2, 256, endpoint=True)
 C, S = np.cos(X), np.sin(X)
 
 plt.plot(X, C)
